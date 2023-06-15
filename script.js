dab4ddfb49c292cf6372a69543942fd7a6619228
alutkovska <alutkovska@gmail.com>
2023-06-15 13:05:33 +0200

    updates to JS

--- script.js
+++ script.js
@@ -43,6 +43,13 @@ let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
   console.log(response.data)
   let temperatureElement = document.querySelector("#temperature");
@@ -71,7 +78,8 @@ function displayTemperature(response) {
 }
 
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily)
   let forecastElement = document.querySelector("#forecast");
  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
   let forecastHTML = `<div class="row">`;
@@ -110,7 +118,6 @@ form.addEventListener("submit", handleSubmit);
 
 
 search("Kyiv");
-displayForecast();
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
